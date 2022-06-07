b7b8d96c26d56771aabf0ee73a3a9d1934c69ccc
Bianca Oura <biancaoura@gmail.com>
2022-06-07 19:39:07 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const agree = document.getElementById('agreement');
 const submitForm = document.getElementById('submit-btn');
 const counter = document.getElementById('counter');
 const txtarea = document.getElementById('textarea');
+const form = document.getElementById('evaluation-form')
 
 const info = document.getElementById('form-data');
 const fName = document.getElementById('input-name');
 const lName = document.getElementById('input-lastname');
 const formMail = document.getElementById('input-email');
 const house = document.getElementById('house');
-const family = document.getElementById('label-family');
+const family = document.getElementsByName('family');
 const content = document.getElementsByClassName('subject');
-const rating = document.getElementById('label-rate');
+const rating = document.getElementsByName('rate');
 
 logBtn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && psw.value === '123456') {
@@ -25,7 +26,6 @@ logBtn.addEventListener('click', () => {
 
 agree.addEventListener('click', (e) => {
   if (e.target.checked) {
-    console.log('o');
     submitForm.disabled = false;
   }
 });
@@ -39,18 +39,49 @@ txtarea.addEventListener('keyup', () => {
 });
 
 function getContent() {
-  
+  const subject = [];
+  for (let i = 0; i < content.length; i += 1) {
+    if (content[i].checked) {
+      subject.push(content[i].value);
+    }
+  }
+  return subject.join(', ');
 }
 
-let data = {
-  Nome: `${fName.value} ${lName.value}`,
-  Email: formMail.value,
-  Casa: house.value,
-  Família: family.value,
-  Matérias: ``
-
+function getRadio(name) {
+  for (let i = 0; i < name.length; i += 1) {
+    if (name[i].checked) {
+      return name[i].value;
+    }
+  }
 }
 
-// submitForm.addEventListener('click', () => {
+function getValues() {
+  const data = {
+    name: `Nome: ${fName.value} ${lName.value}`,
+    email: `Email: ${formMail.value}`,
+    house: `Casa: ${house.value}`,
+    family: `Família: ${getRadio(family)}`,
+    content: `Matérias: ${getContent()}`,
+    rating: `Avaliação: ${getRadio(rating)}`,
+    observation: `Observações: ${txtarea.value}`,
+  };
+  return data;
+}
 
-// })
+function updateInfo(getData) {
+  const keys = Object.values(getData);
+  for (let i = 0; i < keys.length; i += 1) {
+    const para = document.createElement('p');
+    para.innerText = keys[i];
+    info.appendChild(para);
+  }
+}
+
+submitForm.addEventListener('click', (e) => {
+  e.preventDefault();
+  const getData = getValues();
+  getValues();
+  updateInfo(getData);
+  form.style.display = 'none';
+});

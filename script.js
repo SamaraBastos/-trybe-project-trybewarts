75df2a30897a39bb7468482a63c30e91b2399b5c
Bianca Oura <biancaoura@gmail.com>
2022-06-07 17:43:04 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const login = document.getElementById('form');
 const email = document.getElementById('email');
 const psw = document.getElementById('psw');
 const logBtn = document.getElementById('login-btn');
+const agree = document.getElementById('agreement');
+const submitForm = document.getElementById('submit-btn');
 
 logBtn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && psw.value === '123456') {
@@ -9,4 +10,11 @@ logBtn.addEventListener('click', () => {
   } else {
     alert('Email ou senha inválidos.');
   }
-})
+});
+
+agree.addEventListener('click', (e) => {
+  if (e.target.checked) {
+    console.log('o');
+    submitForm.disabled = false;
+  }
+});

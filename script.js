b818cf302d5ef0c3caa0250ab7553cb81f269577
Bianca Oura <biancaoura@gmail.com>
2022-06-07 18:22:08 -0300

    Requisito 21 (parte)

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ const submitForm = document.getElementById('submit-btn');
 const counter = document.getElementById('counter');
 const txtarea = document.getElementById('textarea');
 
+const info = document.getElementById('form-data');
+const fName = document.getElementById('input-name');
+const lName = document.getElementById('input-lastname');
+const formMail = document.getElementById('input-email');
+const house = document.getElementById('house');
+const family = document.getElementById('label-family');
+const content = document.getElementsByClassName('subject');
+const rating = document.getElementById('label-rate');
+
 logBtn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && psw.value === '123456') {
     alert('Olá, Tryber!');
@@ -24,8 +33,24 @@ agree.addEventListener('click', (e) => {
 function countChar() {
   const txtValue = txtarea.value.length;
   counter.innerText = 500 - txtValue;
-  console.log(txtValue);
 }
 txtarea.addEventListener('keyup', () => {
   countChar();
 });
+
+function getContent() {
+  
+}
+
+let data = {
+  Nome: `${fName.value} ${lName.value}`,
+  Email: formMail.value,
+  Casa: house.value,
+  Família: family.value,
+  Matérias: ``
+
+}
+
+// submitForm.addEventListener('click', () => {
+
+// })
